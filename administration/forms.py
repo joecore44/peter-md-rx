aa52e373917fbc3ed016fbbf1fe8c1711c4066af
Joe Shepard <shepard.joseph.m@gmail.com>
2023-01-24 16:57:41 -0700

    added forms for single question submissions

--- administration/forms.py
+++ administration/forms.py
@@ -42,7 +42,6 @@ class ExtendedPatientCreateForm(forms.ModelForm):
 
         ]
 
-
 class PatientUpdateForm(forms.ModelForm):
     
     class Meta:
@@ -80,3 +79,242 @@ class MedicationOrderForm(forms.ModelForm):
             'sharps_container',
             'alcohol_prep_boxes',
         ]
+
+class TestosteroneForm(forms.ModelForm):
+    
+    class Meta:
+        model = MedicationOrder
+        fields = [
+            'testosterone',
+        ]
+class MedicalRequestForm(forms.ModelForm):
+    
+    class Meta:
+        model = MedicationOrder
+        fields = [
+            'medical_requests',
+        ]
+class AdditionalMedicationForm(forms.ModelForm):
+    
+    class Meta:
+        model = MedicationOrder
+        fields = [
+            'additional_medication',
+        ]
+class SignatureRequiredForm(forms.ModelForm):
+    
+    class Meta:
+        model = MedicationOrder
+        fields = [
+            'signature_required',
+        ]
+class SharpsContainerForm(forms.ModelForm):
+        
+        class Meta:
+            model = MedicationOrder
+            fields = [
+                'sharps_container',
+            ]
+class AlcoholPrepBoxesForm(forms.ModelForm):
+        
+        class Meta:
+            model = MedicationOrder
+            fields = [
+                'alcohol_prep_boxes',
+            ]
+class EnclomipheneForm(forms.ModelForm):
+        
+        class Meta:
+            model = MedicationOrder
+            fields = [
+                'enclomiphene',
+            ]
+class HcgForm(forms.ModelForm):
+        
+        class Meta:
+            model = MedicationOrder
+            fields = [
+                'hcg_5000',
+            ]
+class GonadorelinForm(forms.ModelForm):
+        
+        class Meta:
+            model = MedicationOrder
+            fields = [
+                'gonadorelin',
+            ]
+class SemaglutideL1Form(forms.ModelForm):
+        
+        class Meta:
+            model = MedicationOrder
+            fields = [
+                'semaglutide_l1',
+            ]
+class SemaglutideL2Form(forms.ModelForm):
+            
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'semaglutide_l2',
+                ]
+class B12Form(forms.ModelForm):
+            
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'b12_10ml',
+                ]
+class MicB12Form(forms.ModelForm):
+            
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'mic_b12',
+                ]
+class MicOralForm(forms.ModelForm):
+            
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'mic_oral',
+                ]
+class PhentermineForm(forms.ModelForm):
+            
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'phentermine',
+                ]
+class SildenafilForm(forms.ModelForm):
+            
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'sildenafil',
+                ]
+class TadalafilForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'tadalafil',
+                ]
+class EdMaxForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'ed_max',
+                ]
+class FinasterideForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'finasteride',
+                ]
+class HairLossForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'hair_loss',
+                ]
+class DheaForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'dhea',
+                ]
+class PregneloneForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'pregnelone',
+                ]
+class CabergolineForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'cabergoline',
+                ]
+class GlutathioneForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'glutathione',
+                ]
+class SermorelinForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'sermorelin',
+                ]
+class SermorelinPForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'sermorelin_p',
+                ]
+class BpcForm(forms.ModelForm):
+                
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'bpc_157',
+                ]
+class BpcCForm(forms.ModelForm):
+                    
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'bpc_157_c',
+                ]
+class Mk677Form(forms.ModelForm):
+                    
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'mk_677',
+                ]
+class IpaCjcForm(forms.ModelForm):
+                    
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'ipa_cjc',
+                ]
+class IpaForm(forms.ModelForm):
+                    
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'ipa',
+                ]
+class ModafanilForm(forms.ModelForm):
+                    
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'modafanil',
+                ]
+class MetforminForm(forms.ModelForm):
+                    
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'metformin',
+                ]
+class ShippingForm(forms.ModelForm):
+                    
+            class Meta:
+                model = MedicationOrder
+                fields = [
+                    'expedited_shipping',
+                ]
--- administration/views.py
+++ administration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import PatientCreateForm, PatientUpdateForm, ExtendedPatientCreateForm
-from .forms import MedicationOrderForm
+#from .forms import PatientCreateForm, PatientUpdateForm, ExtendedPatientCreateForm
+#from .forms import MedicationOrderForm
+from . import forms
 from .models import PatientProfile, MedicationOrder
 from django.conf import settings
 from django.core.mail import send_mail, EmailMultiAlternatives
@@ -18,10 +19,10 @@ patients_data = [
 
 def staff_home(request):
     patients = PatientProfile.objects.all()
-    pc_form = PatientCreateForm()
+    pc_form = forms.PatientCreateForm()
     
     if request.method == 'POST':
-        pc_form = PatientCreateForm(request.POST)
+        pc_form = forms.PatientCreateForm(request.POST)
         if pc_form.is_valid():
             patient = pc_form.save()
             return redirect('edit-patient', slug=patient.slug)
@@ -33,11 +34,11 @@ def staff_home(request):
 
 def edit_patient(request, slug):
     patient = PatientProfile.objects.get(slug=slug)
-    pc_form = PatientCreateForm(instance=patient)
-    pu_form = PatientUpdateForm(instance=patient)
+    pc_form = forms.PatientCreateForm(instance=patient)
+    pu_form = forms.PatientUpdateForm(instance=patient)
     if request.method == 'POST':
-        pc_form = PatientCreateForm(request.POST, instance=patient)
-        pu_form = PatientUpdateForm(request.POST, instance=patient)
+        pc_form = forms.PatientCreateForm(request.POST, instance=patient)
+        pu_form = forms.PatientUpdateForm(request.POST, instance=patient)
         if pc_form.is_valid():
             patient = pc_form.save()
             return redirect('edit-patient', slug=patient.slug)
@@ -58,12 +59,12 @@ def delete_patient(request, slug):
 
 def patient_fill_form(request, slug, form_slug):
     patient = PatientProfile.objects.get(slug=slug)
-    pc_form = ExtendedPatientCreateForm(instance=patient)
-    mo_form = MedicationOrderForm(instance=patient)
+    pc_form = forms.ExtendedPatientCreateForm(instance=patient)
+    mo_form = forms.MedicationOrderForm(instance=patient)
     
     if request.method == 'POST':
-        pc_form = PatientCreateForm(request.POST, instance=patient)
-        mo_form = MedicationOrderForm(request.POST, instance=patient)
+        pc_form = forms.PatientCreateForm(request.POST, instance=patient)
+        mo_form = forms.MedicationOrderForm(request.POST, instance=patient)
         if pc_form.is_valid():
             patient = pc_form.save()
             return redirect('edit-patient', slug=patient.slug)
@@ -73,7 +74,42 @@ def patient_fill_form(request, slug, form_slug):
     context = {
         'patient': patient,
         'pc_form': pc_form,
-        'mo_form': mo_form
+        'mo_form': mo_form,
+        'testosterone_form': forms.TestosteroneForm,
+        'medical_request_form': forms.MedicalRequestForm,
+        'additional_medication_form': forms.AdditionalMedicationForm,
+        'signature_required_form': forms.SignatureRequiredForm,
+        'sharps_container_form': forms.SharpsContainerForm,
+        'alcohol_prep_boxes_form': forms.AlcoholPrepBoxesForm,
+        'enclomphene_form': forms.EnclomipheneForm,
+        'hcg_form': forms.HcgForm,
+        'gonadorelin_form': forms.GonadorelinForm,
+        'semaglutidel1_form': forms.SemaglutideL1Form,
+        'semaglutidel2_form': forms.SemaglutideL2Form,
+        'b12_form': forms.B12Form,
+        'mic_b12_form': forms.MicB12Form,
+        'mic_oral_form': forms.MicOralForm,
+        'phentermine_form': forms.PhentermineForm,
+        'sildenafil_form': forms.SildenafilForm,
+        'tadalafil_form': forms.TadalafilForm,
+        'ed_max_form': forms.EdMaxForm,
+        'finasteride_form': forms.FinasterideForm,
+        'hair_loss_form': forms.HairLossForm,
+        'dhea_form': forms.DheaForm,
+        'pregnelone_form': forms.PregneloneForm,
+        'cabergoline_form': forms.CabergolineForm,
+        'glutathione_form': forms.GlutathioneForm,
+        'sermorelin_form': forms.SermorelinForm,
+        'sermorelinp_form': forms.SermorelinPForm,
+        'bpc_form': forms.BpcForm,
+        'bpcc_form': forms.BpcCForm,
+        'mk677_form': forms.Mk677Form,
+        'ipa_cjc_form': forms.IpaCjcForm,
+        'ipa_form': forms.IpaForm,
+        'modafanil_form': forms.ModafanilForm,
+        'metformin_form': forms.MetforminForm,
+        'shipping_form': forms.ShippingForm
+
     }
     return render(request, './content/client-front-form-v1.html', context)
 
